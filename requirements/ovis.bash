df45499c290f2553091f272c30c216067745e42d
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2025-03-09 23:31:55 +0000

    fix(ovis): detection of torch had nested ' which python 3.10 doesn't like

--- requirements/ovis.bash
+++ requirements/ovis.bash
@@ -10,7 +10,7 @@ FLASH_ATTN_VERSION="2.7.4.post1"
 # CUDA_VERSION=$(nvcc --version | grep -oP 'release \K[0-9]+')
 CUDA_VERSION=$(nvidia-smi | grep -oP 'CUDA Version: \K[0-9]+\.[0-9]+')
 PYTHON_VERSION=$(python -c 'import sys; print(f"{sys.version_info.major}{sys.version_info.minor}")')
-TORCH_VERSION=$(python -c "import torch; print(f'{torch.__version__.split('.')[0]}.{torch.__version__.split('.')[1]}')");
+TORCH_VERSION=$(python -c "import torch; print(f'''{torch.__version__.split('.')[0]}.{torch.__version__.split('.')[1]}''')");
 FLASH_ATTN_WHEEL="flash_attn-${FLASH_ATTN_VERSION}+cu${CUDA_VERSION}torch${TORCH_VERSION}cxx11abiTRUE-cp${PYTHON_VERSION}-cp${PYTHON_VERSION}-linux_x86_64.whl"
 wget "https://github.com/Dao-AILab/flash-attention/releases/download/v${FLASH_ATTN_VERSION}/$FLASH_ATTN_WHEEL" -P /tmp/ -N
 pip install "/tmp/$FLASH_ATTN_WHEEL"

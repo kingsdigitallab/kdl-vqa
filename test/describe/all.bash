c71302aed76e5649056e1d6c7bdf9322ef47093f
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2025-03-10 00:13:56 +0000

    fix(test): describe/all.bash now limit cuda device to 1st.

--- test/describe/all.bash
+++ test/describe/all.bash
@@ -1,6 +1,6 @@
 cd "$(dirname "$0")"
 bash describer.bash moondream 
-bash describer.bash qwen-vl Qwen/Qwen2.5-VL-3B-Instruct
+CUDA_VISIBLE_DEVICES=1 bash describer.bash qwen-vl Qwen/Qwen2.5-VL-3B-Instruct
 bash describer.bash smol HuggingFaceTB/SmolVLM-Instruct
 bash describer.bash ollama llama3.2-vision
 python3 bvqa.py report -R test/data -t
